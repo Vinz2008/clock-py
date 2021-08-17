b7db2755c05a13fd07bf8b602163cee64996d936
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-17 16:58:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd ~
 mkdir .clock
-mv clock-1.1-linux .clock
+mv clock-gui .clock
 cd .clock
 git clone https://github.com/Vinz2008/clock-py
 cd clock-py
-mv clock.desktop /usr/share/applications/
+sudo mv clock.desktop /usr/share/applications/
